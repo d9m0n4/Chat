1b2dfbb10876b739b15e02c2eccae691f8b2be86
d9m0n4 <c2h8e9d4@gmail.com>
2023-11-17 00:15:11 +0300

    auth

---
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import cls from './Loader.module.scss'
+
+interface LoaderProps {
+ 
+};
+
+export const Loader:FC<LoaderProps> = ({}) => {
+ return (
+  <div>
+   
+  </div>
+ );
+};
